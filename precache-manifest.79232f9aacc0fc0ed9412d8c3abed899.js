e6a3677e84fdcce82c66fda485f288542c3dc874
allan contesini <allancontesini@hotmail.com>
2019-09-15 01:11:15 -0300

    Updates

---
+++ precache-manifest.79232f9aacc0fc0ed9412d8c3abed899.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ad4326e25e665db4362436484442e2",
+    "url": "/hackathonprisma/index.html"
+  },
+  {
+    "revision": "004a667d005c1e0ec7b2",
+    "url": "/hackathonprisma/static/css/2.2fe65d49.chunk.css"
+  },
+  {
+    "revision": "004a667d005c1e0ec7b2",
+    "url": "/hackathonprisma/static/js/2.2ce0f424.chunk.js"
+  },
+  {
+    "revision": "4361dfb0859202099b4e",
+    "url": "/hackathonprisma/static/js/3.9682b14f.chunk.js"
+  },
+  {
+    "revision": "c8ddfdba14a1d34223fb",
+    "url": "/hackathonprisma/static/js/main.9bc1fc6b.chunk.js"
+  },
+  {
+    "revision": "ebce3593595d2a3f07c9",
+    "url": "/hackathonprisma/static/js/runtime~main.a644eee6.js"
+  }
+]);
